841ff44daf5616010d18f75133b765053e990c67
Your Name <you@example.com>
2024-02-29 11:42:29 +0000

    enhancement: added route example html

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use("/example", express.static("example"));
+
 app.listen(5001, () => {
   console.log("Server is listening on Port 5001");
 });

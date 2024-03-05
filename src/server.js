07695e341329b6464864104a7c6f1e57382f32cd
Your Name <you@example.com>
2024-03-05 07:28:49 +0000

    Added basic GET request example

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+// HTTP Verbs - GET, POST, PUT, DELETE
+// Sends GET request
+// const response = await fetch("http://someaddress.com");
+
+// Receives GET request
+app.get("/example", (request, response) => {
+  response.send("Hello there!");
+});
+
 app.listen(5001, () => {
   console.log("Server is listening on Port 5001");
 });

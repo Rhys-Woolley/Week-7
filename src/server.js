7768129d276e0cb2962802ff474375ab6f246f31
Your Name <you@example.com>
2024-03-10 18:43:22 +0000

    added database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const fakeArr = [];
 
 app.use(express.json());
+
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB connection is working");
+};
+
+connection();
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
 
 app.get("/books", (request, response) => {
   response.send({ message: "success", fakeArr: fakeArr });
